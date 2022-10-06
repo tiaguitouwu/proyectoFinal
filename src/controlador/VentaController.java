4cd391d3e86d67f8d39bc138726cb00e0f14bbe3
tiaguito <tiagomartinez2505@gmail.com>
2022-10-06 09:08:18 -0300

    casi todo ventas

--- src/controlador/VentaController.java
+++ src/controlador/VentaController.java
@@ -22,6 +22,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import modelo.factura;
 import modelo.venta;
 
 /**
@@ -54,29 +55,41 @@ public class VentaController implements Initializable {
     @FXML
     private ImageView imagen;
     @FXML
-    private TableView<venta> tablaFactura;
+    private TableView<factura> tablaFactura;
     @FXML
-    private TableColumn<venta, Integer> codFac;
+    private TableColumn<factura, Integer> codFac;
     @FXML
-    private TableColumn<venta, String> productoFac;
+    private TableColumn<factura, String> productoFac;
     @FXML
-    private TableColumn<venta, Integer> cantidadFac;
+    private TableColumn<factura, Integer> cantidadFac;
     @FXML
-    private TableColumn<venta, Integer> precioFac;
+    private TableColumn<factura, Integer> precioFac;
     @FXML
-    private TableColumn<venta, Integer> totalFac;
+    private TableColumn<factura, Integer> totalFac;
     @FXML
     private Label total;
     @FXML
     private Button añadir;
     
-    
+    ObservableList<factura> registros1;
+    int suma,monto;
+    @FXML
+    private TextField precioVenta;
+    @FXML
+    private TextField canVenta;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         cargarDatos();
+        registros1=FXCollections.observableArrayList();
+        productoFac.setCellValueFactory(new PropertyValueFactory<>("producto"));
+        codFac.setCellValueFactory(new PropertyValueFactory<>("codProduc"));
+        cantidadFac.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+        precioFac.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        totalFac.setCellValueFactory(new PropertyValueFactory<>("total"));
+        tablaFactura.setItems(registros1);
     }
     public void cargarDatos(){
         ArrayList<venta> lista=a.consulta();
@@ -93,6 +106,7 @@ public class VentaController implements Initializable {
         venta p=tablaVenta.getSelectionModel().getSelectedItem();
         codVenta.setText(String.valueOf(p.getIdproducto()));
         nomVenta.setText(String.valueOf(p.getNombre()));
+        precioVenta.setText(String.valueOf(p.getPrecio()));
         Image image1=new Image("/recursos/"+p.getImagen());
         imagen.setImage(image1);
        
@@ -102,20 +116,19 @@ public class VentaController implements Initializable {
     private void limpiarTexto(ActionEvent event) {
         codVenta.setText("");
         nomVenta.setText("");
+        precioVenta.setText("");
         Image image1=new Image("/recursos/icono-producto.jpg");
         imagen.setImage(image1);
     }
 
     @FXML
     private void factura(ActionEvent event) {
-        ArrayList<venta> lista=a.consulta();
-        ObservableList<venta> registros=FXCollections.observableArrayList(lista);
-        codFac.setCellValueFactory(new PropertyValueFactory<>("idproducto"));
-        productoFac.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-        precioFac.setCellValueFactory(new PropertyValueFactory<>("precio"));
-        cantidad.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
-        descripcion.setCellValueFactory(new PropertyValueFactory<>("descripcionProducto"));
-        tablaVenta.setItems(registros);
+        int totall=Integer.parseInt(precioVenta.getText())*Integer.parseInt(canVenta.getText());
+        suma=suma+totall;
+        factura local=new factura(nomVenta.getText(),Integer.parseInt(codVenta.getText()),Integer.parseInt(canVenta.getText()),Integer.parseInt(precioVenta.getText()),totall);
+        registros1.add(local);
+        tablaFactura.setItems(registros1);
+        total.setText(String.valueOf(suma));
     }
-    
+   
 }
--- src/modelo/factura.java
+++ src/modelo/factura.java
@@ -10,5 +10,58 @@ package modelo;
  * @author TERCEROS TM
  */
 public class factura {
+    private String producto;
+    private int codProduc, cantidad, precio, total;
+
+    public factura(String producto, int codProduc, int cantidad, int precio, int total) {
+        this.producto = producto;
+        this.codProduc = codProduc;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.total = total;
+    }
+    public factura(){
+    
+    }
+    
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCodProduc() {
+        return codProduc;
+    }
+
+    public void setCodProduc(int codProduc) {
+        this.codProduc = codProduc;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
     
 }
